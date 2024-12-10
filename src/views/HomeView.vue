0fdc607bfef95581df67d054e58d55dac0f2f535
Attila Uygun <auygun@opera.com>
2024-12-10 17:20:29 +0100

    Update home and cleanup

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,90 +1,42 @@
 <script setup lang="ts">
-import WelcomeItem from '../components/WelcomeItem.vue'
-import DocumentationIcon from '../components/icons/IconDocumentation.vue'
-import ToolingIcon from '../components/icons/IconTooling.vue'
-import EcosystemIcon from '../components/icons/IconEcosystem.vue'
-import CommunityIcon from '../components/icons/IconCommunity.vue'
-import SupportIcon from '../components/icons/IconSupport.vue'
+import { ref, watchEffect } from 'vue'
+
+const API_URL = `https://api.github.com/repos/vuejs/core/commits?per_page=3&sha=`
+const branches = ['main', 'minor']
+
+const currentBranch = ref(branches[0])
+const commits = ref([])
+
+watchEffect(async () => {
+  const url = `${API_URL}${currentBranch.value}`
+  commits.value = await (await fetch(url)).json()
+})
+
+function truncate(v) {
+  const newline = v.indexOf('\n')
+  return newline > 0 ? v.slice(0, newline) : v
+}
+
+function formatDate(v) {
+  return v.replace(/T|Z/g, ' ')
+}
 </script>
 
 <template>
-  <WelcomeItem>
-    <template #icon>
-      <DocumentationIcon />
-    </template>
-    <template #heading>Documentation</template>
-
-    Vue’s
-    <a href="https://vuejs.org/" target="_blank" rel="noopener">official documentation</a>
-    provides you with all information you need to get started.
-  </WelcomeItem>
-
-  <WelcomeItem>
-    <template #icon>
-      <ToolingIcon />
-    </template>
-    <template #heading>Tooling</template>
-
-    This project is served and bundled with
-    <a href="https://vite.dev/guide/features.html" target="_blank" rel="noopener">Vite</a>. The
-    recommended IDE setup is
-    <a href="https://code.visualstudio.com/" target="_blank" rel="noopener">VSCode</a>
-    +
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank" rel="noopener">Volar</a>. If
-    you need to test your components and web pages, check out
-    <a href="https://www.cypress.io/" target="_blank" rel="noopener">Cypress</a>
-    and
-    <a href="https://on.cypress.io/component" target="_blank" rel="noopener"
-      >Cypress Component Testing</a
-    >.
-
-    <br />
-
-    More instructions are available in <code>README.md</code>.
-  </WelcomeItem>
-
-  <WelcomeItem>
-    <template #icon>
-      <EcosystemIcon />
-    </template>
-    <template #heading>Ecosystem</template>
-
-    Get official tools and libraries for your project:
-    <a href="https://pinia.vuejs.org/" target="_blank" rel="noopener">Pinia</a>,
-    <a href="https://router.vuejs.org/" target="_blank" rel="noopener">Vue Router</a>,
-    <a href="https://test-utils.vuejs.org/" target="_blank" rel="noopener">Vue Test Utils</a>, and
-    <a href="https://github.com/vuejs/devtools" target="_blank" rel="noopener">Vue Dev Tools</a>. If
-    you need more resources, we suggest paying
-    <a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">Awesome Vue</a>
-    a visit.
-  </WelcomeItem>
-
-  <WelcomeItem>
-    <template #icon>
-      <CommunityIcon />
-    </template>
-    <template #heading>Community</template>
-
-    Got stuck? Ask your question on
-    <a href="https://chat.vuejs.org" target="_blank" rel="noopener">Vue Land</a>, our official
-    Discord server, or
-    <a href="https://stackoverflow.com/questions/tagged/vue.js" target="_blank" rel="noopener"
-      >StackOverflow</a
-    >. You should also subscribe to
-    <a href="https://news.vuejs.org" target="_blank" rel="noopener">our mailing list</a>
-    and follow the official
-    <a href="https://twitter.com/vuejs" target="_blank" rel="noopener">@vuejs</a>
-    twitter account for latest news in the Vue world.
-  </WelcomeItem>
-
-  <WelcomeItem>
-    <template #icon>
-      <SupportIcon />
-    </template>
-    <template #heading>Support Vue</template>
-
-    As an independent project, Vue relies on community backing for its sustainability. You can help
-    us by
-    <a href="https://vuejs.org/sponsor/" target="_blank" rel="noopener">becoming a sponsor</a>.
-  </WelcomeItem>
+  <h1>Latest Vue Core Commits</h1>
+  <template v-for="branch in branches">
+    <input type="radio" :id="branch" :value="branch" name="branch" v-model="currentBranch">
+    <label :for="branch">{{ branch }}</label>
+  </template>
+  <p>vuejs/core@{{ currentBranch }}</p>
+  <ul v-if="commits.length > 0">
+    <li v-for="{ html_url, sha, author, commit } in commits" :key="sha">
+      <a :href="html_url" target="_blank">{{ sha.slice(0, 7) }}</a>
+      - <span>{{ truncate(commit.message) }}</span><br>
+      by <span>
+        <a :href="author.html_url" target="_blank">{{ commit.author.name }}</a>
+      </span>
+      at <span>{{ formatDate(commit.author.date) }}</span>
+    </li>
+  </ul>
 </template>
