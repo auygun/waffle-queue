10e4353ae723673f6459d964da2cdfc68d9f643d
Attila Uygun <auygun@opera.com>
2024-12-27 01:34:58 +0100

    Create db and table

--- server/worker/db.py
+++ server/worker/db.py
@@ -1,15 +1,27 @@
 import aiomysql
 
 
+_create_db = '''
+CREATE DATABASE IF NOT EXISTS builder;
+CREATE TABLE IF NOT EXISTS builder.builds (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    branch TEXT NOT NULL,
+    state ENUM ('REQUESTED', 'BUILDING', 'SUCCEEDED', 'FAILED', 'ABORTED') NOT NULL);
+'''
+
 _pool = None
 
 
 async def open_db():
     print("Opening db")
     global _pool
-    pool = await aiomysql.create_pool(host='127.0.0.1', port=3306,
-                                      user='mysql', password='mysql',
-                                      db=None, autocommit=False)
+    _pool = await aiomysql.create_pool(host='127.0.0.1', port=3306,
+                                       user='mysql', password='mysql',
+                                       db='builder', autocommit=False)
+    # async with _pool.acquire() as db:
+    #     async with db.cursor() as cursor:
+    #         await cursor.execute(_create_db)
+
 
 async def close_db():
     global _pool
@@ -17,6 +29,8 @@ async def close_db():
         print("Closing db")
         _pool.close()
         await _pool.wait_closed()
+        _pool = None
+
 
 def get_db():
     return _pool.acquire()
