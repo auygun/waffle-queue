e8ccab867dae49b903faa77ee8795948ac410b2d
Attila Uygun <auygun@opera.com>
2025-01-19 00:33:13 +0100

    Revert "Checkout and run build script"
    This reverts commit 2ea1e2f635df0d4f349aa62448e926f9e745b3d1.

--- server/worker/worker_daemon.py
+++ server/worker/worker_daemon.py
@@ -68,11 +68,10 @@ class Worker:
                             f"{self._current_build.id()}, "
                             f"branch: {await self._current_build.branch()}")
 
-            worker_dir = Path.home() / "worker"
-            git_dir = worker_dir / "git"
-            work_tree_dir = worker_dir / "work_tree"
+            project_dir = Path.home() / "worker"
+            work_tree_dir = project_dir / "work_tree"
             work_tree_dir.mkdir(parents=True, exist_ok=True)
-            build_script = Path("build") / "build.py"
+            git_dir = project_dir / "git"
 
             try:
                 await self._git.init_or_update(git_dir, "origin",
@@ -82,8 +81,6 @@ class Worker:
                 await self._git.clean(git_dir, work_tree_dir)
                 await self._git.checkout(git_dir, work_tree_dir,
                                          "origin/master")
-                await self._runner.run(["python3", build_script.as_posix()],
-                                       cwd=work_tree_dir)
             except RunProcessError as e:
                 print(e.output.splitlines()[-1])
                 return e.returncode
